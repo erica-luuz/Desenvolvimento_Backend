a9d521f51dff838c7ac188445f8390102159a2d5
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-06-23 23:58:09 -0300

    tentando fazer

--- Lista-1/exercicio01/controleDeEstoqueDeProdutos/controleDeEstoqueDeProdutos/src/main/java/controller/ProdutosController.java
+++ Lista-1/exercicio01/controleDeEstoqueDeProdutos/controleDeEstoqueDeProdutos/src/main/java/controller/ProdutosController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import models.Produto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/produtos")
 
 public class ProdutosController {
-    
+
+    @GetMapping
+    public List<Produto> getAll(){
+
+    }
 }
